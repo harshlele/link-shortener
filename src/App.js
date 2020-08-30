719c8ad5a32c1296a288424e994628e727f2a6bd
Harshal Lele <harshallele97@gmail.com>
2020-08-30 23:09:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
 
 function App() {
+
+  const [shURL, setSHURL] = useState("");
+
+  const onEnterPress = (event) => {
+    if(event.key === "Enter" && event.target.validity.valid){
+      setSHURL("shli.cc/padsf");
+    }
+  };
+
+  const copyURL = (event) => {
+    window.navigator.clipboard.writeText(shURL).then(() => {
+      let url = shURL;
+      setSHURL("Copied to clipboard");
+      setTimeout(() => {
+        setSHURL(url);
+      },2000);
+    })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="title">Link Shortener</div>
+        <div className="input-div">
+          <input type="url" name="link" id="link-input" className="input" placeholder="Paste link and press Enter..." onKeyPress={onEnterPress}/>
+        </div>
+        <div className="result">
+          <div className="sh-link">{shURL}</div>
+          {shURL && <div className="copy-btn" onClick={copyURL}></div>}
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
